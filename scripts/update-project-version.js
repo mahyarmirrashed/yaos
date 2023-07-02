623e8af575899f13107dcd2957884215557c6246
Mahyar Mirrashed <mah.mirr@gmail.com>
2023-07-01 19:08:02 -0500

    ci: add script to update `versions.json` and `manifest.json`

---
+++ scripts/update-project-version.js
@@ -0,0 +1,19 @@
+import { readFileSync, writeFileSync } from "fs";
+import { resolve } from "path";
+
+const packageJsonPath = resolve(__dirname, "..", "package.json");
+const manifestJsonPath = resolve(__dirname, "..", "manifest.json");
+const versionsJsonPath = resolve(__dirname, "..", "versions.json");
+
+const packageJson = JSON.parse(readFileSync(packageJsonPath));
+let manifestJson = JSON.parse(readFileSync(manifestJsonPath));
+let versionsJson = JSON.parse(readFileSync(versionsJsonPath));
+
+const { version: newVersion } = packageJson;
+const { minAppVersion } = manifestJson;
+
+manifestJson.version = newVersion;
+versions[newVersion] = minAppVersion;
+
+writeFileSync(manifestJsonPath, JSON.stringify(manifestJson, null, "  "));
+writeFileSync(versionsJsonPath, JSON.stringify(versionsJson, null, "  "));
