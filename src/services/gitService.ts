dcae94f462ef9dafbea75b2e90734761cd0ff290
Mahyar Mirrashed <mah.mirr@gmail.com>
2023-07-02 09:54:13 -0500

    feat: add basic implementation of git service interface with simple git backend

--- src/services/gitService.ts
+++ src/services/gitService.ts
@@ -1,3 +1,25 @@
+import simpleGit, { SimpleGit } from "simple-git";
+
 export interface GitService {
   isGitInitialized(): Promise<boolean>;
+}
+
+export class SimpleGitService implements GitService {
+  private gitProvider: SimpleGit;
+
+  constructor(private repoPath: string) {
+    this.gitProvider = simpleGit(this.repoPath);
+  }
+
+  async isGitInitialized() {
+    let gitInitialized = true;
+
+    try {
+      await this.gitProvider.revparse(["--is-inside-work-tree"]);
+    } catch (_err) {
+      gitInitialized = false;
+    }
+
+    return gitInitialized;
+  }
 }
