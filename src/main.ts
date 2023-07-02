3c935d4b03a9adf2677d4da0364c4da92b5231d2
Mahyar Mirrashed <mah.mirr@gmail.com>
2023-07-02 09:55:25 -0500

    feat: use git service to check if current vault is configured as a git repository

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,35 @@
-import { Notice, Plugin } from "obsidian";
+import { Notice, Plugin, FileSystemAdapter } from "obsidian";
+import { GitService, SimpleGitService } from "@/services/gitService";
 
 const PLUGIN_ICON = "sync";
 const PLUGIN_NAME = "YAOS";
 
 export default class YaosPlugin extends Plugin {
+  private gitService?: GitService;
+
   async onload() {
-    this.addRibbonIcon(PLUGIN_ICON, PLUGIN_NAME, (_evt: MouseEvent) => {
-      new Notice("Clicked!");
-    });
+    this.gitService = new SimpleGitService(this.getBasePath());
+
+    this.addRibbonIcon(
+      PLUGIN_ICON,
+      PLUGIN_NAME,
+      this.handleRibbonIconClick.bind(this)
+    );
+  }
+
+  getBasePath(): string {
+    return (this.app.vault.adapter as FileSystemAdapter).getBasePath();
+  }
+
+  async handleRibbonIconClick(_evt: MouseEvent) {
+    if (!this.gitService) {
+      return;
+    }
+
+    if (await this.gitService.isGitInitialized()) {
+      new Notice("Vault is initialized as a Git repository.");
+    } else {
+      new Notice("Vault is not initialized as a Git repository.");
+    }
   }
 }
