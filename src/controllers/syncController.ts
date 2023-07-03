90eef2bc8156271843ba50e606a0859d5b19b581
Mahyar Mirrashed <mah.mirr@gmail.com>
2023-07-03 03:21:06 -0500

    feat: complete vault backup process

--- src/controllers/syncController.ts
+++ src/controllers/syncController.ts
@@ -2,6 +2,8 @@ import { GitService } from "@/services/gitService";
 import { GitignoreService } from "@/services/gitignoreService";
 import logger from "@/utils/logger";
 
+import { Notice } from "obsidian";
+
 export default class SyncController {
   constructor(
     private gitService: GitService,
@@ -9,6 +11,52 @@ export default class SyncController {
   ) {
     logger.debug("Initialized sync controller.");
   }
+
+  private async createVaultBackup(): Promise<void> {
+    if (await this.gitService.unstagedChangesExist()) {
+      await this.handleUnstagedChanges();
+    } else {
+      logger.info("No changes in vault detected for backup.");
+    }
+
+    if (await this.gitService.isLocalAhead()) {
+      await this.handleLocalAhead();
+    }
+  }
+
+  private async handleLocalAhead() {
+    try {
+      await this.gitService.gitPush();
+
+      logger.success("Created vault backup.");
+    } catch {
+      logger.warn("Remote and local have conflicting changes!");
+      logger.info("Starting rebase process...");
+
+      try {
+        await this.gitService.gitPullWithRebase();
+        await this.gitService.gitPush();
+
+        logger.success("Created vault backup.");
+      } catch {
+        logger.error("Automatic rebasing failed.");
+        this.notifyUserAboutConflicts();
+      }
+    }
+  }
+
+  private async handleUnstagedChanges() {
+    logger.info("Unstaged changes detected. Creating backup...");
+
+    await this.gitService.gitStageAll();
+    await this.gitService.gitCommit();
+  }
+
+  private notifyUserAboutConflicts(): void {
+    new Notice("Your local and remote repositories had conflicting changes.");
+    new Notice("Please fix the changes and then click the sync button again.");
+  }
+
   async sync(): Promise<void> {
     if (await this.gitService.isGitInitialized()) {
       logger.debug("Vault is initialized as a Git repository.");
@@ -28,5 +76,7 @@ export default class SyncController {
 
     if (await this.gitService.isRebasing())
       await this.gitService.stopRebasing();
+
+    await this.createVaultBackup();
   }
 }
