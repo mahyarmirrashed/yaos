4a18d6f64bf46e174395d7b57cbc5ca8825fcae2
Mahyar Mirrashed <mah.mirr@gmail.com>
2023-07-10 22:22:20 -0500

    refactor: remove plugin name prefix for command
    Obsidian will automatically prefix command identifications with the plugin identification.

--- src/main.ts
+++ src/main.ts
@@ -28,7 +28,7 @@ export default class YaosPlugin extends Plugin {
     );
 
     this.addCommand({
-      id: `${PLUGIN_NAME.toLowerCase()}-show-unmerged`,
+      id: `show-unmerged`,
       name: "Show unmerged/conflicting files",
       callback: () => new UnmergedFilesView(this.app, this.gitService).open(),
     });
